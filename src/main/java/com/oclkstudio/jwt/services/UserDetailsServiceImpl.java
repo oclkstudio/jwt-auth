fdc7cbe3ed90aa03ae2d00f08a093131484398b5
Omer CELIK <o.clk@outlook.com.tr>
2020-04-12 22:59:11 +0300

    Initial commit

---
+++ src/main/java/com/oclkstudio/jwt/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.oclkstudio.jwt.services;
+
+import com.oclkstudio.jwt.model.User;
+import com.oclkstudio.jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with -> username or email : " + username)
+                );
+
+        return UserPrinciple.build(user);
+    }
+}
